b253e5215ca44137a042c2ad4e15f586e7bcc9ba
MertzAndreas <Andreasmertz11@gmail.com>
2026-05-12 15:47:18 +0200

    SymbolTable

--- include/stochastic/symbol_table.hpp
+++ include/stochastic/symbol_table.hpp
@@ -1,6 +1,25 @@
-
 #pragma once
+#include <concepts>
+#include <map>
 
 namespace stochastic {
-class SymbolTable {};
+template <typename K, typename V>
+  requires std::equality_comparable<K>
+class SymbolTable {
+private:
+  std::map<K, V> symbols;
+
+public:
+  void add(K key, V val) {
+    if (symbols.contains(key))
+      throw new std::exception();
+    symbols[key] = val;
+  }
+
+  V get(K key) {
+    if (!symbols.contains(key))
+      throw new std::exception();
+    return symbols[key];
+  }
+};
 } // namespace stochastic

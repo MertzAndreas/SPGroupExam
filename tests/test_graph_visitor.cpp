fab08925ce377e4d8222206eabc1a93f92a6d2fe
MertzAndreas <Andreasmertz11@gmail.com>
2026-05-16 12:39:30 +0200

    Added testing

---
+++ tests/test_graph_visitor.cpp
@@ -0,0 +1,50 @@
+#include "stochastic/vessel.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <format>
+
+bool ToDotCorrectly() {
+  auto v = stochastic::Vessel{"Test Vessel"};
+  const double lambda = 0.001;
+  const auto A = v.add("A", 1);
+  const auto B = v.add("B", 1);
+  const auto C = v.add("C", 1);
+
+  v.add((A + C) >> lambda >>= B + C);
+
+  std::ostringstream stream;
+  v.to_dot(stream);
+
+  auto string = stream.str();
+
+  // Check each reactant name is in the string
+  auto containsReactantIds = string.contains("label=\"A\"") &&
+                             string.contains("label=\"B\"") &&
+                             string.contains("label=\"C\"");
+
+  // Check the rate is in the string on the format r<id>
+  auto containsRateValue = string.contains("label=\"0.001\"");
+
+  // Check we have input -> rate transition
+  // Check we have rate -> output transition
+  auto containsTransitionIds =
+      string.contains(std::format("s{} -> r", A.getId())) &&
+      string.contains(std::format("s{} -> r", C.getId())) &&
+      string.contains(std::format("-> s{}", B.getId())) &&
+      string.contains(std::format("-> s{}", C.getId()));
+
+  return containsReactantIds && containsRateValue && containsTransitionIds;
+}
+
+bool ToDotEmpty() {
+  auto v = stochastic::Vessel{"Test Vessel"};
+  std::ostringstream stream;
+  v.to_dot(stream);
+
+  auto string = stream.str();
+  return string.contains("digraph {\n}\n");
+}
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+  REQUIRE(ToDotCorrectly());
+  REQUIRE(ToDotEmpty());
+}
--- tests/test_symbol_table.cpp
+++ tests/test_symbol_table.cpp
@@ -1,2 +1,34 @@
+#include "stochastic/symbol_table.hpp"
+#include <catch2/catch_test_macros.hpp>
 
-int main() {}
+void ThrowsOn2ConsecutiveInsert() {
+  stochastic::SymbolTable<int, int> test_symbol_table;
+  test_symbol_table.add(1, 1);
+  test_symbol_table.add(1, 2);
+}
+
+void ThrowsOnGetThatIsntThere() {
+  stochastic::SymbolTable<int, int> test_symbol_table;
+  test_symbol_table.get(1);
+}
+
+bool InsertsWithoutThrow() {
+  stochastic::SymbolTable<int, int> test_symbol_table;
+  test_symbol_table.add(1, 1);
+  test_symbol_table.add(2, 2);
+  return true;
+}
+
+bool InsertAndGetsCorrectly() {
+  stochastic::SymbolTable<int, int> test_symbol_table;
+  auto insertedValue = 1;
+  test_symbol_table.add(1, insertedValue);
+  return insertedValue == test_symbol_table.get(1);
+}
+
+TEST_CASE("Symbol table methods") {
+  REQUIRE_THROWS(ThrowsOn2ConsecutiveInsert());
+  REQUIRE_THROWS(ThrowsOnGetThatIsntThere());
+  REQUIRE(InsertsWithoutThrow());
+  REQUIRE(InsertAndGetsCorrectly());
+}
